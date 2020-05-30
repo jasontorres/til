17908e1b4434c59e8e115ab4b0cca8b720c99421
Jason Torres <jason.e.torres@gmail.com>
2020-05-30 09:09:28 +0800

    Fixes svelte page loader

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,58 +1,71 @@
 <script context="module">
-	import { onMount } from 'svelte';
-	import fetch from 'node-fetch';
-	import marked from 'marked';
+  import { onMount } from "svelte";
+  import fetch from "node-fetch";
+  import marked from "marked";
 
-	import {siteName, datoToken} from '../../config';
+  import { siteName, datoToken } from "../../config";
 
-	let posts = [];
+  let posts = [];
 
   export const preload = async (page, session) => {
-    fetch("https://graphql.datocms.com/", {
+    await fetch("https://graphql.datocms.com/", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
         Accept: "application/json",
-        Authorization: `Bearer ${datoToken}`,
+        Authorization: `Bearer ${datoToken}`
       },
       body: JSON.stringify({
-        query: "{ allPosts { title body slug _publishedAt } }",
-      }),
-    })
-      .then((res) => res.json())
-      .then((res) => {
-        posts = res.data.allPosts;
+        query: "{ allPosts { title body slug _publishedAt } }"
       })
+    })
+      .then(res => res.json())
+      .then(res => {
+        posts = res.data.allPosts;
+      });
   };
 
   function formatDate(date) {
-    return date.getDate() + "/" + (date.getMonth() + 1) + "/" + date.getFullYear()
+    return (
+      date.getDate() + "/" + (date.getMonth() + 1) + "/" + date.getFullYear()
+    );
   }
 </script>
 
+<style>
+
+</style>
+
 <main>
-  <h1>{siteName}
-  <span>by Jason Torres</span>
+  <h1>
+    {siteName}
+    <span>by Jason Torres</span>
   </h1>
 
   {#each posts as { title, body, slug, _publishedAt }, i}
-  <section>
-    <a hrel='prefetch' href='post/{slug}'><h2>{title}</h2></a>
-    <p>{@html marked(body)}</p>
-    <p>Posted: {formatDate(new Date(_publishedAt))}</p>
-  </section>
-  <hr />
+    <section>
+      <a hrel="prefetch" href="post/{slug}">
+        <h2>{title}</h2>
+      </a>
+      <p>
+        {@html marked(body)}
+      </p>
+      <p>Posted: {formatDate(new Date(_publishedAt))}</p>
+    </section>
+    <hr />
   {:else}
-  <section><center>Loading posts</center></section>
+    <section>
+      <center>Loading posts</center>
+    </section>
   {/each}
 
 </main>
 
 <footer>
-&copy; Jason Torres of <a href='https://www.mashupgarage.com'>Mashup Garage</a>. 
-<br/>
-This site is built on Svelte, NewCSS, and DatoCMS. Source code is available at <a href="https://github.com/jasontorres/til">Github.</a>
+  &copy; Jason Torres of
+  <a href="https://www.mashupgarage.com">Mashup Garage</a>
+  .
+  <br />
+  This site is built on Svelte, NewCSS, and DatoCMS. Source code is available at
+  <a href="https://github.com/jasontorres/til">Github.</a>
 </footer>
-
-<style>
-</style>
---
+++ src/routes/post/[slug].svelte
@@ -0,0 +1,83 @@
+<script context="module">
+  import { onMount } from "svelte";
+  import fetch from "node-fetch";
+  import marked from "marked";
+
+  import { siteName, datoToken } from "../../../config";
+
+  export async function preload(page, session) {
+    const { slug } = page.params;
+    const result = await fetch("https://graphql.datocms.com/", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+        Authorization: `Bearer ${datoToken}`
+      },
+      body: JSON.stringify({
+        query: `query getPost {
+        post(filter: {slug: {eq: "${slug}"}}) {
+          body
+          id
+          slug
+          title
+          updatedAt
+          createdAt
+          published
+          _publishedAt
+          _seoMetaTags(locale: en) {
+            attributes
+            content
+            tag
+          }
+        }
+      }`
+      })
+    }).then(res => res.json());
+
+    const post = result.data.post;
+
+    return { post };
+  }
+
+  function formatDate(date) {
+    return (
+      date.getDate() + "/" + (date.getMonth() + 1) + "/" + date.getFullYear()
+    );
+  }
+</script>
+
+<script>
+  export let post;
+</script>
+
+<!-- <svelte:head>
+  <title>{post.title}</title>
+</svelte:head>
+ -->
+<main>
+  <h1>
+    {siteName}
+    <span>by Jason Torres</span>
+  </h1>
+
+  <section>
+    <a hrel="prefetch" href="post/{post.slug}">
+      <h2>{post.title}</h2>
+    </a>
+    <p>
+      {@html marked(post.body)}
+    </p>
+    <p>Posted: {formatDate(new Date(post._publishedAt))}</p>
+  </section>
+
+</main>
+
+<footer>
+  &copy; Jason Torres of
+  <a href="https://www.mashupgarage.com">Mashup Garage</a>
+  .
+  <br />
+  This site is built on Svelte, NewCSS, and DatoCMS. Source code is available at
+  <a href="https://github.com/jasontorres/til">Github.</a>
+</footer>
