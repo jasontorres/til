2a57b51e5c59b34ffd271454170b6ea3c97ea7ff
Jason Torres <jason.e.torres@gmail.com>
2020-05-31 11:01:45 +0800

    Format for posts

--- src/routes/post/[slug].svelte
+++ src/routes/post/[slug].svelte
@@ -2,6 +2,7 @@
   import { onMount } from "svelte";
   import fetch from "node-fetch";
   import marked from "marked";
+  import prismjs from "prismjs";
 
   import { siteName, datoToken } from "../../../config";
 
@@ -45,6 +46,29 @@
       date.getDate() + "/" + (date.getMonth() + 1) + "/" + date.getFullYear()
     );
   }
+
+
+  const renderer = new marked.Renderer();
+  renderer.code = function(code, lang, escaped) {
+    code = this.options.highlight(code, lang);
+    if (!lang) {
+      return `<pre><code>${code}</code></pre>`;
+    }
+
+    var langClass = "language-" + lang;
+    return `<pre class="${langClass}"><code class="${langClass}">${code}</code></pre>`;
+  };
+
+  marked.setOptions({
+    renderer,
+    highlight: function(code, lang) {
+      try {
+        return prismjs.highlight(code, prismjs.languages[lang], lang);
+      } catch {
+        return code;
+      }
+    }
+  });
 </script>
 
 <script>
